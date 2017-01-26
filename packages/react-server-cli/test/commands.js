d359d5068110d0f6421d7980ed781c826a3304ab
Karl Horky <karl.horky@gmail.com>
2017-01-26 18:56:56 +0100

    Clean up

--- packages/react-server-cli/test/commands.js
+++ packages/react-server-cli/test/commands.js
@@ -9,13 +9,7 @@ import rimraf from 'rimraf';
 const fixturesPath = path.join(__dirname, 'fixtures', 'commands');
 
 fs.readdirSync(fixturesPath).forEach(testName => {
-	// process.stdout.write(testName);
 	if (testName[0] === '.') return;
-
-
-
-	// FIXME: Remove!
-	// if (testName !== 'start-basic' && testName !== 'start-missing-routesz') return;
 
 	test(`${testName} command`, async t => {
 		const testPath = path.join(fixturesPath, testName);
@@ -32,8 +26,6 @@ fs.readdirSync(fixturesPath).forEach(testName => {
 			stdoutIncludes,
 			stderrIncludes
 		} = JSON.parse(fs.readFileSync(path.join(testPath, 'options.json')));
-		// process.chdir(`${__dirname}/fixtures/commands/start-basic/in-files`);
-
 
 		const server = child_process.spawn(
 			process.execPath,
@@ -52,16 +44,6 @@ fs.readdirSync(fixturesPath).forEach(testName => {
 		const frequency = 100;
 		let elapsed = 0;
 
-		if (stdoutIncludes) {
-			process.stdout.write(`${testName} stdoutIncludes\n`)
-			process.stdout.write(stdoutIncludes)
-			process.stdout.write('\n')
-		} else if (stderrIncludes) {
-			process.stdout.write(`${testName} stderrIncludes\n`)
-			process.stdout.write(stderrIncludes);
-			process.stdout.write('\n');
-		}
-
 		await new Promise(resolve => {
 			const checkForExpectedStdout = setInterval(() => {
 				if (!stdout.includes(stdoutIncludes) && elapsed < 5000) return elapsed += frequency;
@@ -69,13 +51,6 @@ fs.readdirSync(fixturesPath).forEach(testName => {
 				resolve();
 			}, frequency);
 		});
-
-		// FIXME: Remove!
-		process.stdout.write(`\n${testName} stdout\n`);
-		process.stdout.write(stdout);
-		process.stdout.write(`\n${testName} stderr\n`);
-		process.stdout.write(stderr);
-
 
 		if (stdoutIncludes) t.true(stdout.includes(stdoutIncludes), 'stdout includes expected output');
 		if (stderrIncludes) t.true(stderr.includes(stderrIncludes), 'stderr includes expected output');
@@ -92,10 +67,6 @@ function changeToTempDir(tmpPath){
 	fs.mkdirSync(tmpPath);
 	process.chdir(tmpPath);
 }
-
-// clear();
-// outputFileSync('foo', 'bar');
-// process.stdout.write(fixturesPath);
 
 function noDotDirectory(x){
 	return x !== '.';
